72eccb79a859baf6e0584479e87262fe903622cd
unknown <luizadelaja@gmail.com>
2016-03-18 16:29:38 +0000

    More features

--- Main.php
+++ Main.php
@@ -3,17 +3,17 @@
 		<title>Visiting Log</title>
 	</head>
 	<body>
-		<form action="<Main.php" method="GET">
+		<form action="Main.php" method="GET">
 			<fieldset>
-				<legend>Adding Visiting Information</legend>
-					Date: <input type="date" name="visitdate"/><br>
-					Student ID: <input type="text" name="studId"/><br>
-					User ID: <input type="text" name="userId"/><br>
-					Observations: <input type="textarea" name="observe"/><br>
-					Vitals: <input type="textarea" name="vitalsinfo"/><br>
-					Symptoms: <input type="textarea" name="symptoms"/><br>
-					Prescriptions: <input type="textarea" name="prescripts"/><br>
-				<input type="submit" onclick='alert('Connection Established!')' value="Add">
+				<legend><h3>Adding Visiting Information</h3></legend>
+					Date: <br><input type="date" name="visitdate"/><br>
+					Student ID: <br><input type="text" name="studId"/><br>
+					User ID: <br><input type="text" name="userId"/><br>
+					Observations: <br><textarea rows="4" cols="50" name="observation" form="usrform">Enter text here...</textarea><br>
+					Vitals: <br><textarea rows="4" cols="50" name="vitalinfo" form="usrform">Enter text here...</textarea><br>
+					Symptoms: <br><textarea rows="4" cols="50" name="symptoms" form="usrform">Enter text here...</textarea><br>
+					Prescriptions: <br><textarea rows="4" cols="50" name="prescripts" form="usrform">Enter text here...</textarea><br>
+				<input type="submit" value="Add">
 			</fieldset>
 		</form>	
 	</body>
@@ -33,6 +33,36 @@
 	}
 	else{
 		echo "Connection established";
+	}
+
+
+	if(!isset($_REQUEST['username'])){
+		exit();		//if no data, exit
+	}
+
+	$vid="default";
+	$pid=$_REQUEST['studId'];
+	$uid=$_REQUEST['userId'];
+	$dateofvisit=$_REQUEST['visitdate'];
+	$observe=$_REQUEST['observation'];
+	$vitals=$_REQUEST['vitalinfo'];
+	$symptoms=$_REQUEST['symptoms'];
+	$prescripts=$_REQUEST['prescripts'];
+
+	$strQuery="insert into visitlog values
+				VID='$vid',
+				PID='$pid',
+				UID='$uid',
+				DateOfVisit='$dateofvisit',
+				Observations='$observe',
+				VitalsInfo='$vitals',
+				Symptoms='$symptoms',
+				Prescriptions='$prescripts'";
+
+	if($db->query($strQuery)){
+		echo "Data Added";
+	}else{
+		echo "Error while adding data";
 	}
 	//Master code for the project
 ?>
