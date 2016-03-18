549f79a3688a1123cb323ce3614b7b2e4ec2e496
unknown <luizadelaja@gmail.com>
2016-03-18 15:39:28 +0000

    Connection to the database

--- Main.php
+++ Main.php
@@ -1,3 +1,34 @@
+<html>
+	<head>
+		<title>Visiting Log</title>
+	</head>
+	<body>
+		<form action="adduser.php" method="GET">
+			Username: <input type="text" name="username"/>
+			User Group: <select name="usergroup">
+				<option value="1">Admin</option>
+				<option value="2">Students</option>
+				<option value="3">Faculty</option>
+			</select>
+			<input type="submit" value="Add">
+		</form>	
+	</body>
+</html>
+
 <?php
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$databasename="ashclinicgroup12";
+
+	$db = new mysqli($servername, $username, $password, $databasename);
+	if($db->connect_errno){
+		//no connection, exit
+		echo "Cannot Connect";
+		exit();
+	}
+	else{
+		echo "Connection established";
+	}
 	//Master code for the project
 ?>
