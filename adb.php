2bccfecf03927dbbf1aefc7dc1b54296a3831b48
Youssouf da Silva <youssoufdasilva@gmail.com>
2016-04-26 19:07:32 +0000

    Solved merge issues with db.php

--- adb.php
+++ adb.php
@@ -1,19 +1,11 @@
 <?php
-<<<<<<< HEAD
 
 include_once("setting.php");
-=======
-/**
-*Database connection helper
-*/
-include_once("setting1.php");
 
->>>>>>> origin/addPersonAjax
 /**
 * Database connection helper class
 */
 class adb{
-<<<<<<< HEAD
 	
 	var $db=null;
 	var $result=null;
@@ -29,39 +21,18 @@ class adb{
 		
 		//connect
 		$this->db=new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
-=======
-	var $db=null;
-	var $result=null;
-	function adb(){
-	}
-	/**
-	*Connect to database
-	*@return boolean true if connected else false
-	*/
 
-	function connect(){
-
-		//connect
-		$this->db=new mysqlI(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
->>>>>>> origin/addPersonAjax
 		if($this->db->connect_errno){
 			//no connection, exit
 			return false;
 		}
 		return true;
 	}
-<<<<<<< HEAD
 	
 	/**
 	*Query the database 
 	*@param string $strQuery sql string to execute
 	*@return boolean true if connected else false
-=======
-
-	/**
-	*Query the database
-	*@param string $strQuery sql string to execute
->>>>>>> origin/addPersonAjax
 	*/
 	function query($strQuery){
 		if(!$this->connect()){
@@ -76,7 +47,6 @@ class adb{
 		}
 		return true;
 	}
-<<<<<<< HEAD
 	
 	/**
 	*Fetch from the current data set and return
@@ -92,41 +62,7 @@ class adb{
 			return false;
 		}
 		
-=======
-	/*
-	* Fetch from the current data set and return
-	*@return array one record
-	*/
-	function fetch(){
-		//Complete this funtion to fetch from the $this->result
-		if($this->result==null){
-			return false;
-		}
-
-		if($this->result==false){
-			return false;
-		}
-
->>>>>>> origin/addPersonAjax
 		return $this->result->fetch_assoc();
 	}
 }
-/*
-This is a test code
-$obj=new adb();
-<<<<<<< HEAD
-if(!$obj->query("select * from UsersInfo"))
-=======
-if(!$obj->query("select * from personsinfo"))
->>>>>>> origin/addPersonAjax
-{
-	echo "error";
-	exit();
-}
-print_r($obj->fetch());
-*/
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> origin/addPersonAjax
